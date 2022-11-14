2d5a113fed3c59f1654b5da9c6492c908d0fe83a
AnnaLind <annalindtner@t-online.de>
2022-11-14 00:00:36 -0500

    Update script3.js

--- lec9/script3.js
+++ lec9/script3.js
@@ -3,7 +3,7 @@ function addFunction(){
   var number2 = document.getElementById("number2").value;
   var rightNumber1 = Number(number1);
   var rightNumber2 = Number(number2);
-  var addition = number1 + number2;
+  var addition = rightNumber1 + rightNumber2;
 
   document.getElementById("output").innerHTML= addition;
 }
